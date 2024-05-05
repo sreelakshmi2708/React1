20e8cf72b05933ccc5f72bc753400d474a787de3
sreelakshmi2708 <sreelakshmi2708@gmail.com>
2024-05-05 11:24:13 +0530

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY BOSS 🥳😎🥳!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY BOSS 🥳😎🥳!</h3>
+        <p>Dear Boss,</p>
+        <p>
+         Wishing you continued success, positivity, and happiness always. Stay chill as always. May all of your wishes come true.
+        </p>
+         Support Team ❤️
+      </div>
+    </div>
+  );
+  
